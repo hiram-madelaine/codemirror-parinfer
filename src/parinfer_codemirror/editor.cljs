aff1a4befe0740a4ddf03844dede377e6ad8d305
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2015-12-10 11:16:36 +0100

    Suppress all vcr related code

--- src/parinfer_codemirror/editor.cljs
+++ src/parinfer_codemirror/editor.cljs
@@ -10,9 +10,7 @@
                                           IEditor
                                           get-prev-state
                                           frame-updated?
-                                          set-frame-updated!
-                                          ;record-change!
-                                          ]]))
+                                          set-frame-updated!]]))
 
 ;;----------------------------------------------------------------------
 ;; Life Cycle events
@@ -40,7 +38,6 @@
   "Called after any change is applied to the editor."
   [cm change]
   (when (not= "setValue" (.-origin change))
-    #_(record-change! cm {:change (parse-change change)})     ;TODO VCR
     (fix-text! cm :change change)
     (update-cursor! cm change)
     (set-frame-updated! cm true)))
@@ -49,7 +46,6 @@
   "Called after the cursor moves in the editor."
   [cm]
   (when-not (frame-updated? cm)
-    #_(record-change! cm {:selections (parse-selections (.listSelections cm))}) ;TODO VCR
     (fix-text! cm))
   (set-frame-updated! cm false))
 
@@ -93,7 +89,6 @@
      (let [element (js/document.getElementById element-id)
            cm (js/CodeMirror.fromTextArea element (clj->js (merge editor-opts opts)))
            wrapper (.getWrapperElement cm)
-           ;watcher (js/scrollMonitor.create wrapper)
            initial-state (assoc empty-editor-state
                                 :mode (:parinfer-mode opts))
            prev-editor-state (atom nil)]
@@ -101,32 +96,13 @@
 
        (set! (.-id wrapper) (str "cm-" element-id))
 
-       #_(when-not (:readOnly opts)                           ;TODO VCR
-
-         ;; on blur, start playing animation again, if we are not dev mode.
-         (.on cm "blur" (fn [e]                             ;TODO VCR
-                          (when-not (:show? @controls-state)
-                            (when (.-isInViewport watcher)
-                              (play-recording! key-)))))
-
-         ;; on focus, set recording controls to focus on this editor.
-         ;; and stop any animation.
-         (.on cm "focus" (fn [e]                            ;TODO VCR
-                           (swap! controls-state assoc :target-key key-)
-                           (stop-playing! key-)
-                           (on-cursor-activity cm))))
-
        (when-not (get @state key-)
          (swap! frame-updates assoc key- {}))
 
        (swap! state update-in [key-]
               #(-> (or % initial-state)
-                   (assoc :cm cm
-                          ;:watcher watcher
-                          )))
+                   (assoc :cm cm)))
 
-       #_(swap! vcr update-in [key-]                          ;TODO VCR
-              #(or % empty-recording))
 
        ;; Extend the code mirror object with some utility methods.
        (specify! cm
@@ -134,18 +110,7 @@
                  (get-prev-state [this] prev-editor-state)
                  (cm-key [this] key-)
                  (frame-updated? [this] (get-in @frame-updates [key- :frame-updated?]))
-                 (set-frame-updated! [this value] (swap! frame-updates assoc-in [key- :frame-updated?] value))
-                 #_(record-change! [this new-thing]           ;TODO VCR
-                                 (let [data (get @vcr key-)]
-                                   (when (:recording? data)
-                                     (let [last-time (:last-time data)
-                                           now (.getTime (js/Date.))
-                                           dt (if last-time (- now last-time) 0)
-                                           new-changes (conj (:changes data) (assoc new-thing :dt dt))
-                                           new-data (assoc data
-                                                           :last-time now
-                                                           :changes new-changes)]
-                                       (swap! vcr assoc key- new-data))))))
+                 (set-frame-updated! [this value] (swap! frame-updates assoc-in [key- :frame-updated?] value)))
 
        ;; handle code mirror events
        (.on cm "change" on-change)
